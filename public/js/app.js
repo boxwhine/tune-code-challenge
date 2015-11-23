9a031246b043cd0a1ce6e1ba4d7ef407c8df5cd1
Jesse Rinehart <jesse.rinehart@arrayhealth.com>
2015-11-22 22:12:00 -0800

    Add/wire up all routes with static partial templates

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,43 @@
 var app = angular.module('tune-app', [
     'ngRoute'
 ]);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/products.html',
+            controller: 'ProductCtrl'
+        })
+        .when('/products', {
+            templateUrl: 'partials/products.html',
+            controller: 'ProductCtrl'
+        })
+        .when('/products/:id', {
+            templateUrl: 'partials/product-detail.html',
+            controller: 'ProductCtrl'
+        })
+        .when('/cart', {
+            templateUrl: 'partials/cart.html',
+            controller: 'CartCtrl'
+        })
+        .when('/404', {
+            templateUrl: '404.html'
+        })
+        .otherwise({
+            redirectTo: '/404'
+        });
+}]);
+
+
+app.controller('ProductCtrl', function ( $scope, $location, $http, $routeParams ) {
+    var msg = 'Product Controller reporting for duty.';
+    $scope.productId = $routeParams.id;
+    if($routeParams.id) {
+        msg = 'You\'re looking at product number ' + $routeParams.id;
+    }
+    console.log(msg);
+});
+
+app.controller('CartCtrl', function ( $scope, $location, $http ) {
+    console.log('Cart Controller reporting for duty.');
+});
